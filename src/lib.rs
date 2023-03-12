8ccd882bc92cad03012dc8066bde2f97bf91d817
Carson M <carson@pyke.io>
2023-03-11 22:32:46 -0600

    chore: add license headers

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+// Copyright 2022-2023 pyke.io
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 //! <img src="https://parcel.pyke.io/v2/cdn/assetdelivery/diffusers/doc/diffusers.png" width="100%" alt="pyke Diffusers">
 //!
 //! `pyke-diffusers` is a modular library for pretrained diffusion model inference using [ONNX Runtime], inspired by
--- src/schedulers/mod.rs
+++ src/schedulers/mod.rs
@@ -1,3 +1,17 @@
+// Copyright 2022-2023 pyke.io
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 //! The schedule functions, denoted *Schedulers* in the library, take in the output of a trained model, a sample which
 //! the diffusion process is iterating on, and a timestep, returning a denoised sample.
 //!
